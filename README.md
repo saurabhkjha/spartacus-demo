d9952b4464fbd905a8486dd869ca48df85c59f25
saurabhkjha <saurabh.simpy@gmail.com>
2020-07-14 18:30:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-6jipry
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-6jipry)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ProductDetailOutlets } from '@spartacus/storefront'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  pdpOutlets = ProductDetailOutlets ;
+}
